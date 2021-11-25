9df514f7b792963a577615bfcfb5113bb8eb003d
Henrique Morikawa <henriqueyukiharu@gmail.com>
2021-11-24 22:31:30 -0300

    initial setup

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,14 @@
+export function RepositoryList() {
+  return (
+    <section>
+      <h1>Lista de repositório</h1>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
