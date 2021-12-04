94bccea1d990a51f56d64468dfda1968531fd4fa
Henrique Morikawa <henriqueyukiharu@gmail.com>
2021-12-04 14:40:28 -0300

    add listing stylying

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,20 @@
+import { RepositoryItem } from "./RespositoryItem";
+
 export function RepositoryList() {
+  const repository = {
+    name: "unform",
+    description: "Forms in React",
+    url: "https://github.com/unform/unform",
+  };
+
   return (
     <section>
       <h1>Lista de repositório</h1>
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">Acessar repositório</a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
