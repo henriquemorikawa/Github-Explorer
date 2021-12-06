a422fb2f2c79cac4d50127917ff97e16397ed240
Henrique Morikawa <henriqueyukiharu@gmail.com>
2021-12-06 19:38:33 -0300

    add typing

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,8 +2,14 @@ import { RepositoryItem } from "./RespositoryItem";
 import { useState, useEffect } from "react";
 import "../styles/repositories.scss";
 
+interface Repositories {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repositories[]>([]);
 
   useEffect(() => {
     fetch("https://api.github.com/orgs/rocketseat/repos")
